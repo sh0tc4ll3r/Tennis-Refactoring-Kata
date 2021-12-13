003ee73b61074de42dffcaaa2229002635c86de0
DF Lorenzo <three@hey.com>
2021-12-13 01:48:40 +0100

    Added Kata result and readme based on it.

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,80 +2,91 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
-
-        public TennisGame1(string player1Name, string player2Name)
-        {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
-        }
+        private int _mScore1;
+        private int _mScore2;
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                _mScore1 += 1;
             else
-                m_score2 += 1;
+                _mScore2 += 1;
         }
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (_mScore1 == _mScore2)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
+                return EqualsScore();
+            }
 
-                }
-            }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            if (_mScore1 >= 4 || _mScore2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                return Advantages();
             }
+
+            return GeneralGameFlow("");
+        }
+
+        private string GeneralGameFlow(string score)
+        {
+            for (var i = 1; i < 3; i++)
+            {
+                score = ScoreTally(score, i);
+            }
+
+            return score;
+        }
+
+        private string ScoreTally(string score, int i)
+        {
+            int tempScore;
+            if (i == 1) tempScore = _mScore1;
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+                score += "-";
+                tempScore = _mScore2;
             }
+
+            switch (tempScore)
+            {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
+            }
+
             return score;
+        }
+
+        private string Advantages()
+        {
+            return (_mScore1 - _mScore2) switch
+            {
+                1 => "Advantage player1",
+                -1 => "Advantage player2",
+                >= 2 => "Win for player1",
+                _ => "Win for player2"
+            };
+        }
+
+        private string EqualsScore()
+        {
+            return _mScore1 switch
+            {
+                0 => "Love-All",
+                1 => "Fifteen-All",
+                2 => "Thirty-All",
+                _ => "Deuce"
+            };
         }
     }
 }
